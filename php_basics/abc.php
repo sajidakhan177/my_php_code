fc7644f9cdfee2b94086b75e9c3e06dc67574d32
sajidakhan177 <57210862+sajidakhan177@users.noreply.github.com>
2023-01-24 11:31:35 +0530

    Add files via upload

--- php_basics/abc.php
+++ php_basics/abc.php
@@ -1,1 +1,38 @@
+<?php
+/*
+$x = 5;#global scope
 
+function myfunc(){
+    echo "x = $x";
+
+}
+
+#local scope
+function myfunc()
+{
+    $x = 10;
+    echo "<p>Variable x inside function is: $x</p>"."<br>";
+
+#PHP The global Keyword
+#The global keyword is used to access a global variable from within a function.
+
+$x = 20;
+function myfunc()
+
+{
+   global $x;
+    echo "<p>Variable x inside function is: $x</p>"."<br>";
+}*/
+
+
+function myTest() {
+  static $x = 0;
+  echo $x;
+  $x++;
+}
+
+myTest();
+myTest();
+myTest();
+myTest();
+?>
